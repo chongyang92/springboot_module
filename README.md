49bc4045c98838c69e11fa76e3c0dc4732f55556
Administrator <1454102071@qq.com>
2020-01-17 02:51:17 +0800

    1、模块化:springboot和uidGenerator两个模块 2、集成uidGenerator

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # springboot_model分为多个模块
 ## uid-generator模块
     **_全局唯一ID生成器_**
-    介绍详见 https://github.com/baidu/uid-generator/blob/master/README.zh_cn.md
-    集成到springboot中详见 https://www.jianshu.com/p/947bff7be2da
-    父工程和子模块创建详见 https://blog.csdn.net/wnf2018/article/details/80655153
+    1. 介绍详见 https://github.com/baidu/uid-generator/blob/master/README.zh_cn.md
+    2. 集成到springboot中详见 https://www.jianshu.com/p/947bff7be2da
+    及 https://blog.csdn.net/m0_37367413/article/details/87341352
+    https://blog.csdn.net/paincupid/article/details/89605554
+    注：使用cached-uid-spring.xml注入bean
+    3. 父工程和子模块创建详见 https://blog.csdn.net/wnf2018/article/details/80655153
+    4. 同一工程不同模块之间的调用详见 https://blog.csdn.net/menggudaoke/article/details/90601008
 ## 1.spring boot模块  
 **_不同分支是不同的集成环境_**
 ### 1.1 master分支
--- springboot/src/main/java/com/weboot/springboot/controller/rbac/OrgController.java
+++ springboot/src/main/java/com/weboot/springboot/controller/rbac/OrgController.java
@@ -1,10 +1,12 @@
 package com.weboot.springboot.controller.rbac;
 
+import com.baidu.fsg.uid.UidGenerator;
 import com.weboot.springboot.controller.param.OrgValidator;
 import com.weboot.springboot.model.Org;
 import com.weboot.springboot.service.OrgService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,9 +22,14 @@ public class OrgController {
 
     @Resource
     private OrgService orgService;
+    @Resource
+    private UidGenerator uidGenerator;
 
     @RequestMapping(value = "/list",method = RequestMethod.POST)
     public List<Org> login(@RequestBody OrgValidator orgValidator, BindingResult bindingResult){
+
+        System.out.println(uidGenerator.getUID());
+
         return orgService.getOrglist();
     }
 }
